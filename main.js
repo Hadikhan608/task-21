ff4b213dbe9fd9eab8ff2fd0d41738b80090f5ec
Abdul Hadi <161719851+Hadikhan608@users.noreply.github.com>
2024-06-08 13:34:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+//Task 21: Thing of something you could store in a typescript object
+//write a program that creates objects containing these items.
+let car = {
+    colour: "Red",
+    company: "Picanto",
+    Color: "Blue",
+    Model: "2022",
+};
+console.log(car);
